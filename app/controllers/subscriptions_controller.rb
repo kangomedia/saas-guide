2ca8dd073cc94c831b4bd511518b428bd6d2852a
Freddy Rodriguez <SaulARodriguez@a2zmacbook.local>
2016-05-08 14:28:29 -0500

    Saved stripe plan id to Account model and set redirect and notice on successful subscription.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,7 @@
 class SubscriptionsController < ApplicationController
 
+	before_action :authenticate_user!
+
 	def new
 		@plans = Plan.all
 	end
@@ -20,6 +22,14 @@ class SubscriptionsController < ApplicationController
 		  :plan => plan,
 		  :email => email
 		)
+
+		# Customer created with a valid subscription
+		# So, update Account model
+		account = Account.find_by_email(current_user.email)
+		account.stripe_plan_id = plan
+		account.save!
+
+		redirect_to :root, :notice => "Successfuly subscribed to a plan"
 
 		rescue => e
 			redirect_to :new_subscription, :flash => { :error => e.message }
